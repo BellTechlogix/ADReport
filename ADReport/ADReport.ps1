c938a52f6ccaf533dbd5bf32d1f0e5a53277d5cb
tankcr <kristopher.roy@gmail.com>
2017-10-06 14:19:09 -0700

    Updated
    Updated Folder paths to output dynamic
    Updated imports to only grab whats need to save mem

--- ADReport/ADReport.ps1
+++ ADReport/ADReport.ps1
@@ -2,7 +2,7 @@
 ADReport.ps1
 Created By Kristopher Roy
 Date Created 25Jan15
-Date Modified 05Oct17
+Date Modified 06Oct17
 Script purpose - Gather details about an Active Directory Environment
 #>
 
@@ -193,25 +193,28 @@ Return $Script:x
 #Check for Pre-Existing CSVDE Report
 $csvdechoice = $NULL
 $csvdechoice = user-prompt -Title "CSVDE Report Status" -Message "Do you already have an exported CSVDE Report that you wish to use?" -Selection1ToolTip "Says that you already have an exported report that you wish to use." -Selection2ToolTip "Tells the system you need a report."
-If($csvdechoice -eq 0){$ADExport = Import-Csv (Get-FileName -Title "Select Your CSVDExport file" -Filter "csv")}
+If($csvdechoice -eq 0){
+	$file = Get-FileName -Title "Select Your CSVDExport file" -Filter "csv" -obj
+	#$ADExport = Import-Csv $file
+}
 #FullADReport
 #CSVDE Is a comprehensive AD tool that pulls a whole lot of AD Data
 If($csvdechoice -eq 1 -or $ADExport -eq $NULL)
 {
 	#Folder Location for Reports
 	$folder = get-folder -Description "Select folder you wish to place reports in"
-	Write-Host "No CSVDE import found Gathering CSVDE Report"
+	Write-Host "No CSVDE import found Gathering CSVDE Report this may take some time"
 	CSVDE -F $folder\CSVDEADExport.csv
 	#Import CSVDE CSV Report as Object
-	$ADExport = Import-Csv $folder\CSVDEADExport.csv
+	#$ADExport = Import-Csv $folder\CSVDEADExport.csv
+	$file = "$folder\CSVDEADExport.csv"
 }
-
+IF($folder -eq $null -or $folder -eq ""){$folder = $file.DirectoryName}
 #Select Report Types You Wish
 $options01 = "Users","Machines","Both"
 $reportselection = MultipleSelectionBox -listboxtype one -inputarray $options01
 
-$dcou = "ou=domain controllers,dc=ngkacu,dc=com"
-$dcs = Get-ADComputer -searchbase $dcou -filter '*'|where-object{$_.name -ne "DC1"}
+$dcs = Get-ADDomainController -Filter *
 
 #deffinition for UAC codes
 $lookup = @{4096="Workstation/Server"; 4098="Disabled Workstation/Server"; 4128="Workstation/Server No PWD"; 
@@ -225,7 +228,7 @@ $time = Get-Date
 #Select Only Machines
 IF($reportselection -eq "Machines" -or $reportselection -eq "Both")
 {
-	$computers = $ADExport|where-object{$_.objectClass -eq "computer"}|Select-Object -Property Name,operatingSystem,operatingSystemVersion,operatingSystemServicePack,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,description,cn,DN,memberOf,badPasswordTime,pwdLastSet,accountExpires,IPv4Address
+	$computers = Import-Csv $file|where-object{$_.objectClass -eq "computer"}|Select-Object -Property Name,operatingSystem,operatingSystemVersion,operatingSystemServicePack,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,description,cn,DN,memberOf,badPasswordTime,pwdLastSet,accountExpires,IPv4Address
 	#Doublecheck and format AD Report for PC's
 	FOREACH ($computer in $computers)
 	{
@@ -242,7 +245,7 @@ IF($reportselection -eq "Machines" -or $reportselection -eq "Both")
 		ELSE{try{$computer.accountExpires = Get-Date $PC.accountExpires -format "M/d/yyyy hh:mm tt"}Catch{}}
 		$computer.userAccountControl = $lookup[$PC.userAccountControl]
 	}
-	$computers|export-csv c:\belltech\ComputerExport.csv -NoTypeInformation
+	$computers|export-csv $folder\ComputerExport.csv -NoTypeInformation
 	#Workstation Count By OS, Version, and Service Pack
 	$resultsarray =@()
 	$Vers = $computers|select-object operatingSystemVersion,OperatingSystem,OperatingSystemServicePack -unique
@@ -259,17 +262,20 @@ IF($reportselection -eq "Machines" -or $reportselection -eq "Both")
 		$OSobject|add-member -MemberType NoteProperty -name "SP" -value ($curOS|select-object OperatingSystemServicePack -Unique).operatingSystemServicePack
 		$resultsarray += $OSobject
 	}
-	$resultsarray|export-csv c:\belltech\WorkstationCount.csv -NoTypeInformation -Encoding UTF8
+	$resultsarray|export-csv $folder\WorkstationCount.csv -NoTypeInformation -Encoding UTF8
 }
 
 #Select Only Users
 IF($reportselection -eq "Users" -or $reportselection -eq "Both")
 {
-	$users = $ADExport|where-object{$_.objectClass -eq "user"}|Select-Object -Property SamAccountName,givenName,sn,telephoneNumber,mobile,mail,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,description,office,City,cn,DN,memberOf,badPasswordTime,pwdLastSet,LockedOut,accountExpires
+	$users = Import-Csv $file|where-object{$_.objectClass -eq "user"}|Select-Object -Property SamAccountName,givenName,sn,telephoneNumber,mobile,mail,userAccountControl,whenCreated,whenChanged,lastlogondate,dayssincelogon,description,office,City,cn,DN,memberOf,badPasswordTime,pwdLastSet,LockedOut,accountExpires
 	#Doublecheck and format AD Report for Users
+	$i = 0
 	FOREACH ($user in $users)
 	{
-		$curuser = $DCs |% { Get-ADUser -Server $_.DNSHostName $user.sAMAccountName -Properties * } | sort-object -property lastLogondate| select -Last 1
+		$i++
+		Write-Progress -Activity ("Gathering User Date . . ."+$user.SamAccountName) -Status "Scanned: $i of $($users.Count)" -PercentComplete ($i/$users.Count*100)
+		$curuser = $DCs |% { Get-ADUser -Server $_.HostName $user.sAMAccountName -Properties * } | sort-object -property lastLogondate| select -Last 1
 		$user.lastlogondate = Try{Get-Date $curuser.lastlogondate -format "M/d/yyyy hh:mm tt"}catch{}
 		try{$user.dayssincelogon = (New-TimeSpan -start $user.lastlogondate -end $time).days}catch{$user.dayssincelogon = "Null"}
 		$user.whenCreated = Try{Get-Date $curuser.whenCreated -format "M/d/yyyy hh:mm tt"}catch{}
@@ -280,16 +286,16 @@ IF($reportselection -eq "Users" -or $reportselection -eq "Both")
 		ELSE{try{$user.accountExpires = Get-Date $curuser.accountExpires -format "M/d/yyyy hh:mm tt"}Catch{}}
 		$user.userAccountControl = $lookup[$curuser.userAccountControl] 
 	}
-	$users|export-csv c:\belltech\UserExport.csv -NoTypeInformation
+	$users|export-csv $folder\UserExport.csv -NoTypeInformation
 	#Account Counts
 	$accountsarray =@()
 	$Countobject = New-Object PSObject
 	$Countobject|add-member -MemberType NoteProperty -name "UserCount" -value $users.count
-	$Countobject|add-member -MemberType NoteProperty -name "Groups" -value ($ADExport|where-object{$_.objectClass -eq "group"}).count
-	$Countobject|add-member -MemberType NoteProperty -name "Ou's/Containers" -value (($ADExport|where-object{$_.objectClass -eq "container"}).count + ($ADExport|where-object{$_.objectClass -eq "organizationalUnit"}).count)
+	$Countobject|add-member -MemberType NoteProperty -name "Groups" -value (Import-Csv $file|where-object{$_.objectClass -eq "group"}).count
+	$Countobject|add-member -MemberType NoteProperty -name "Ou's/Containers" -value ((Import-Csv $file|where-object{$_.objectClass -eq "container"}).count + ($ADExport|where-object{$_.objectClass -eq "organizationalUnit"}).count)
 	$Countobject|add-member -MemberType NoteProperty -Name "Disabled Users" -Value ($users|where-object{$_.userAccountControl -ilike "Disabled*"}).count
 	$Countobject|add-member -MemberType NoteProperty -Name "Users Pwd Not Expire" -Value ($users|where-object{$_.userAccountControl -ilike "*PWD Not Expire"}).count
 	$Countobject|add-member -MemberType NoteProperty -Name "Users No Logon last 30" -Value (($users|where-object{$_.dayssincelogon -ge 30}).count)
 	$accountsarray += $Countobject
-	$accountsarray|export-csv c:\belltech\AccountStructureCount.csv -NoTypeInformation -Encoding UTF8
+	$accountsarray|export-csv $folder\AccountStructureCount.csv -NoTypeInformation -Encoding UTF8
 }
